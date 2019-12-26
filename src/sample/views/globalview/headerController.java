dd9f956eb1a0314e33fad1caace47bbaa631d8d9
Nprime496 <nnanejunior@gmail.com>
2019-12-26 21:05:53 +0100

    h

--- src/sample/views/globalview/headerController.java
+++ src/sample/views/globalview/headerController.java
@@ -1,4 +1,23 @@
 package sample.views.globalview;
 
-public class headerController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class headerController implements Initializable {
+    @FXML
+    public ImageView imageViewHeader;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imageViewHeader.setImage(
+                new Image(getClass().getResource("poussin.jpg").toString())
+        );
+
+    }
 }
