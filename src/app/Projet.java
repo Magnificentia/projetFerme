c8bdd0ca2538dc2ce9a03a2a523232fd3b85b274
Nprime496 <nnanejunior@gmail.com>
2020-02-19 11:51:50 +0100

    modification nom commande to informations commercial

--- src/app/Projet.java
+++ src/app/Projet.java
@@ -129,10 +129,10 @@ public class Projet extends Application {
         IOption accueil=new OptionItem("Accueil","views/accueil/accueilView.fxml");
         IOption client=new OptionItem("Client","views/client/clientView.fxml");
         IOption catalogue=new OptionItem("Catalogue","views/catalogue/catalogueView.fxml");
-        IOption commande=new OptionItem("commande","views/commande/commandeView.fxml");
-        IOption parametres=new OptionItem("commande","views/parametres/parametresView.fxml");
+        IOption commande=new OptionItem("commandefdfasfasdfsad","views/commande/commandeView.fxml");
+        IOption parametres=new OptionItem("Parametres","views/parametres/parametresView.fxml");
         IOption infoselevage=new OptionItem("infos elevage","views/infosElevage/eleveurView.fxml");
-        IOption infoscommercial=new OptionItem("commande","views/infosCommercial/commercialView.fxml");
+        IOption infoscommercial=new OptionItem("Infos commercial","views/infosCommercial/commercialView.fxml");
         
         List elevage=new ArrayList<>();//options menu elevage
         elevage.add(infoselevage);
